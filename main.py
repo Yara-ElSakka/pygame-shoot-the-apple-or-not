0a02f19c66ed1e3d7b76ae6b1f320edd017decb2
Yara ElSakka <95961383+Yara-ElSakka@users.noreply.github.com>
2022-03-11 22:19:46 +0400

    Initial commit

---
+++ main.py
@@ -0,0 +1,47 @@
+# shoot the fruit game (confusing) and with more info by yara
+
+from random import randint
+from pgzero.builtins import *
+
+apple = Actor("apple")
+orange = Actor("orange")
+pineapple = Actor("pineapple")
+
+
+def draw():
+    screen.clear()
+    apple.draw()
+    pineapple.draw()
+    orange.draw()
+
+
+def place_apple():
+    apple.x = randint(10, 800)
+    apple.y = randint(10, 600)
+
+
+def place_orange():
+    orange.x = randint(10, 800)
+    orange.x = randint(10, 800)
+
+
+def place_pineapple():
+    pineapple.x = randint(10, 800)
+    pineapple.x = randint(10, 800)
+
+
+def on_mouse_down(pos):
+    if pineapple.collidepoint(pos):
+        print("Good shot!")
+        place_apple()
+        place_orange()
+        place_pineapple()
+    else:
+        print("sorry ... not sorry !!! You missed!")
+        quit()
+
+
+place_apple()
+place_orange()
+place_pineapple()
+# end of code 11.03.22
